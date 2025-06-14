b9a317624be763d227433b685db43e06a7910430
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-06-14 21:19:48 +0700

    Add 404 error handler to render custom 404 page

--- app.py
+++ app.py
@@ -220,5 +220,9 @@ def predictions():
 def rangkuman():
     return render_template('rangkuman.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
