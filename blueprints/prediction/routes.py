953963cba223d53b3e1af26e1a166cfac7745871
Muhamad Ibnu Khaidar Hafiz <45196310+Cleign1@users.noreply.github.com>
2025-06-20 20:13:34 +0700

    Remove prediction blueprint routes
    Deleted the routes.py file for the prediction blueprint, removing the '/prediksi-stok' route and its associated view.

--- blueprints/prediction/routes.py
+++
@@ -1,8 +0,0 @@
-# blueprints/prediction/routes.py
-from flask import Blueprint, render_template
-
-prediction_bp = Blueprint('prediction', __name__)
-
-@prediction_bp.route('/prediksi-stok')
-def prediksi_stok():
-    return render_template('prediksi_stok.html')
