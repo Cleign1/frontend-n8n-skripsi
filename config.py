408914ad94a6b85cc3b6caeec0b32803ea0ebfb9
Muhamad Ibnu Khaidar Hafiz <ibnukhaidar@gmail.com>
2025-10-12 22:10:13 +0700

    feat: Add workflow definitions for stock update process

--- config.py
+++ config.py
@@ -25,6 +25,33 @@ class Config:
     N8N_SUMMARY_WEBHOOK_URL = os.getenv("N8N_SUMMARY_WEBHOOK_URL")
     WORKFLOW_2 = os.getenv("WORKFLOW_2")
 
+    # WORKFLOW_DEFINITIONS
+    WORKFLOWS = {
+        'update': {
+            'title': 'Proses Update Stok',
+            'steps': [
+                {'id': os.getenv("UPDATE_STEP_1_ID"), 'name': 'Webhook Trigger', 'description': 'Webhook Trigger'},
+                {'id': os.getenv("UPDATE_STEP_2_ID"), 'name': 'Process CSV', 'description': 'Mencari file CSV berdasarkan tanggals'},
+                {'id': os.getenv("UPDATE_STEP_3_ID"), 'name': 'Download CSV', 'description': 'Mendownload file CSV'},
+                {'id': os.getenv("UPDATE_STEP_4_ID"), 'name': 'Extract CSV File', 'description': 'Ekstrak file CSV'},
+                {'id': os.getenv("UPDATE_STEP_5_ID"), 'name': 'Prepare Bulk Query', 'description': 'Siapkan kueri massal untuk pembaruan stok'},
+                {'id': os.getenv("UPDATE_STEP_6_ID"), 'name': 'Eksekusi Kueri SQL', 'description': 'Mengeksekusi kueri SQL untuk memperbarui stok di database'},
+            ]
+        },
+        # 'prediction': {
+        #     'title': 'Proses Prediksi Stok',
+        #     'steps': [
+        #         {'id': os.getenv("PREDICTION_STEP_1_ID"), 'name': 'Webhook Trigger', 'description': 'Trigger untuk memulai prediksi stok'},
+        #     ]
+        # },
+        # 'report': {
+        #     'title': 'Proses Pembuatan Laporan',
+        #     'steps': [
+        #         {'id': os.getenv("REPORT_STEP_1_ID"), 'title': 'Menerima Panggilan Webhook', 'description': 'Alur kerja laporan dipicu.'},
+        #     ]
+        # }
+    }
+
     # Celery configuration
     CELERY_BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
     result_backend = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
